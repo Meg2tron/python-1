1b64414db4df43250d2644883585c6d67c8af580
tbhaxor <tbhaxor@gmail.com>
2017-11-15 05:56:45 +0530

    armstrong number checker

---
+++ code/armstrong_number/armstrong_number.py
@@ -0,0 +1,13 @@
+
+def is_armstrong(number):
+    # defining total
+    total = 0
+
+    # cloning number
+    duplicate = number
+    while duplicate != 0:
+        total += (duplicate % 10) ** 3
+        duplicate /= 10
+
+    # returns true if sum of cube of digits is equal to original number
+    return total == number
---
+++ code/armstrong_number/test.py
@@ -0,0 +1,16 @@
+import unittest
+import armstrong_number
+
+class Armstrong_Number(unittest.TestCase):
+
+    def testing_armstrong(self):
+        self.assertEqual(armstrong_number.is_armstrong(371), True)
+        pass
+
+    def testing_non_armstrong(self):
+        self.assertEqual(armstrong_number.is_armstrong(370), False)
+        pass
+
+    pass
+
+unittest.main()
