93c7477abbb22fc235c5bfce0786a2720fec7af5
tbhaxor <tbhaxor@gmail.com>
2017-11-15 10:51:03 +0530

    modified test.py :bug:

--- code/sum_of_digits/test.py
+++ code/sum_of_digits/test.py
@@ -3,23 +3,23 @@ import sum_digits
 
 class Sum_Digits(unittest.TestCase):
     def testing_case1(self):
-        self.assertEqual(sum_digits.sum_digits(123), 6)
+        self.assertEqual(sum_digits.sum_of_digits(123), 6)
         pass
 
     def testing_case2(self):
-        self.assertEqual(sum_digits.sum_digits(0), 0)
+        self.assertEqual(sum_digits.sum_of_digits(0), 0)
         pass
 
     def testing_case3(self):
-        self.assertEqual(sum_digits.sum_digits(4588), 25)
+        self.assertEqual(sum_digits.sum_of_digits(4588), 25)
         pass
 
     def testing_case4(self):
-        self.assertEqual(sum_digits.sum_digits(1), 1)
+        self.assertEqual(sum_digits.sum_of_digits(1), 1)
         pass
 
     def testing_case5(self):
-        self.assertEqual(sum_digits.sum_digits(-1), None)
+        self.assertEqual(sum_digits.sum_of_digits(-1), None)
         pass
 
     pass
